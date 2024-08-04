b7dc1e386306611ad7e6a0cf786b3e2cce519a69
Jon Karlsen <karlsen.jon@icloud.com>
2024-08-04 17:06:17 +0900

    feat: multiple PING

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,49 @@
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{BufRead, BufReader, BufWriter, Write}, net::{TcpListener, TcpStream}};
+
+
+const ADDRESS: &str    = "127.0.0.1";
+const PORT   : &str    = "6379";
+const PONG   : &[ u8 ] = b"+PONG\r\n";
+
+
+fn handle_request( stream: &mut TcpStream ) {
+    let     stream_clone = stream.try_clone().unwrap();
+    let mut reader       = BufReader::new( stream );
+    let mut writer       = BufWriter::new( stream_clone );
+    let mut buf: Vec<u8> = Vec::new();
+
+    loop {
+        let bytes_read = reader.read_until( b'\n', &mut buf ).unwrap();
+
+        if bytes_read == 0 {
+            break;
+        }
+
+        let line = String::from_utf8( buf.clone() ).unwrap();
+
+        println!( "{}", line );
+
+        if line.trim() == "PING" {
+            writer.write_all( PONG ).unwrap();
+            writer.flush().unwrap();
+        }
+
+        buf.clear();
+    }
+}
 
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!( "Logs from your program will appear here!" );
+    let listener = TcpListener::bind( format!( "{}:{}", ADDRESS, PORT ) ).unwrap();
 
-     let listener = TcpListener::bind( "127.0.0.1:6379" ).unwrap();
-
-     for stream in listener.incoming() {
-         match stream {
-             Ok( mut stream ) => {
-                    let response = b"+PONG\r\n";
-                    let mut buf  = [ 0; 1024 ];
-                    let     _    = stream.read( &mut buf );
-
-                    if String::from_utf8_lossy( &buf ).contains( "PING" ) {
-                        let _ = stream.write_all( response ).expect( "write failed" );
-                    }
-
-                    stream.flush().expect( "flush failed" );
-             }
-             Err( e ) => {
-                 println!( "error: {}", e );
-             }
-         }
-     }
+    for stream in listener.incoming() {
+        match stream {
+            Ok( mut stream ) => {
+                handle_request( &mut stream );
+            }
+            Err( e ) => {
+                println!( "error: {}", e );
+            }
+        }
+    }
 }
